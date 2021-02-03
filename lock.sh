f2912284ac3bf8db18c4f0537e93070e6a52afb6
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-02-03 19:11:32 +0200

    Update scrot usage

--- lock.sh
+++ lock.sh
@@ -1,5 +1,5 @@
-#scrot lock.png
-#convert lock.png -blur 0x6 out_lock.png
+scrot -o lock.png
+convert lock.png -blur 0x6 out_lock.png
 # while sleep 20s; do xdotool mousemove 10 10; xdotool mousemove 20 20; done &
-#i3lock -i out_lock.png
-i3lock -i ~/.xmonad/lockscreen.png
+i3lock -i out_lock.png
+#i3lock -i ~/.xmonad/lockscreen.png
